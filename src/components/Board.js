edf397e8131bcd869dd057bfb52f11a3a63a4bef
Andrew Wang <andrewzlwang@outlook.com>
2024-01-09 10:17:43 -0800

    filled board with king

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,29 @@
 import React from "react";
 import Square from "./Square";
+import K from "../assets/K.png"
 
 export default function Board(props) {
     let squares = [];
     let row = [];
+    const startingBoardState = [["r", "n", "b", "q", "k", "b", "n", "r"],
+                                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                                [" ", " ", " ", " ", " ", " ", " ", " "],
+                                [" ", " ", " ", " ", " ", " ", " ", " "],
+                                [" ", " ", " ", " ", " ", " ", " ", " "],
+                                [" ", " ", " ", " ", " ", " ", " ", " "],
+                                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                                ["R", "N", "B", "Q", "K", "B", "N", "R"]];
+    const [boardState, setBoardState] = React.useState(startingBoardState);
+
+    console.log(boardState);
 
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            row.push(<Square id = {8*i + j + 1} bgcolor = {((i + j) % 2 == 0)? "#B58863" : "#F0D9B5" }/>);
+            row.push(<Square id = {8*i + j + 1} 
+                bgcolor = {((i + j) % 2 != 0)? "#B58863" : "#F0D9B5" } 
+                piece = {K}/>);
         }
-        squares.push(<div>{row}</div>);
+        squares.push(<div style={{margin: '0px', padding: '0px', border: '0px'}}>{row}</div>);
         row = [];
     }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -4,12 +4,20 @@ export default function Square(props) {
     let id = props.id;
 
     return (
-        <button className="square" style={{backgroundColor: props.bgcolor,  
-        height: '100px', 
-        width: '100px', 
+        <button className="square" style={{
+        backgroundColor: props.bgcolor,  
+        height: '64px', 
+        width: '64px', 
         outline: 'none', 
         border: 'none'}}>
-            {id}
+            <img src={props.piece} alt="" style={{
+            position: 'relative',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            height: '58px',
+            width: '58px',
+            }}/>
         </button>
     );
 }
