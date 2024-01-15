59252a098ba50fb0150da24adab82336ed92e2b1
Andrew Wang <andrewzlwang@outlook.com>
2024-01-14 22:43:56 -0800

    checking working kinda

--- src/components/Board.js
+++ src/components/Board.js
@@ -97,48 +97,48 @@ export default function Board(props) {
             if (containsColorPiece(i, !white)) oppSquares.push(i);
         }
         for (let i = 0; i < oppSquares.length; i++) {
-            possibleMoves = possibleMoves.concat(getMoves(oppSquares[i], !white));
+            possibleMoves = possibleMoves.concat(getMoves(oppSquares[i], !white, true));
         }
         for (let i = 0; i < possibleMoves.length; i++) {
-            if (bdState[possibleMoves[i] - 1].toLowerCase() === 'k') return true;
+            if (bdState[possibleMoves[i] - 1] === (white? 'K' : 'k')) return true;
         }
         return false;
     }
 
-    // returns the ids of squares that are valid pawn moves from the given id.
-    const getPawnMoves = (id, white) => {
+    // returns the ids of squares that are valid pawn moves from the given id. 
+    const getPawnMoves = (id, white, checking) => {
         let moves = [];
         if (white) {
-            if (id - 8 > 0 && !containsColorPiece(id - 8, true) && !containsColorPiece(id - 8, false)) {
+            if (!checking && id - 8 > 0 && !containsColorPiece(id - 8, true) && !containsColorPiece(id - 8, false)) {
                 moves.push(id - 8);
             }
-            if ((id % 8 !== 1) && containsColorPiece(id - 9, false)) {
+            if ((id % 8 !== 1) && (containsColorPiece(id - 9, false) || checking)) {
                 moves.push(id - 9);
             }
-            if ((id % 8 !== 0) && containsColorPiece(id - 7, false)) {
+            if ((id % 8 !== 0) && (containsColorPiece(id - 7, false) || checking)) {
                 moves.push(id - 7);
             }
-            if (49 <= id && id <= 56 && !containsColorPiece(id - 16, true) && !containsColorPiece(id - 16, false) && moves.includes(id - 8)) {
+            if (!checking && 49 <= id && id <= 56 && !containsColorPiece(id - 16, true) && !containsColorPiece(id - 16, false) && moves.includes(id - 8)) {
                 moves.push(id - 16);
             }
         } else {
-            if (id + 8 <= 64 && !containsColorPiece(id + 8, true) && !containsColorPiece(id + 8, false)) {
+            if (!checking && id + 8 <= 64 && !containsColorPiece(id + 8, true) && !containsColorPiece(id + 8, false)) {
                 moves.push(id + 8);
             }
-            if ((id % 8 !== 1) && containsColorPiece(id + 7, true)) {
+            if ((id % 8 !== 1) && (containsColorPiece(id + 7, true) || checking)) {
                 moves.push(id + 7);
             }
-            if ((id % 8 !== 0) && containsColorPiece(id + 9, true)) {
+            if ((id % 8 !== 0) && (containsColorPiece(id + 9, true) || checking)) {
                 moves.push(id + 9);
             }
-            if (9 <= id && id <= 16 && !containsColorPiece(id + 16, true) && !containsColorPiece(id + 16, false) && moves.includes(id + 8)) {
+            if (!checking && 9 <= id && id <= 16 && !containsColorPiece(id + 16, true) && !containsColorPiece(id + 16, false) && moves.includes(id + 8)) {
                 moves.push(id + 16);
             }
         }
         return moves;
     }
 
-    const getRookMoves = (id, white) => {
+    const getRookMoves = (id, white, checking) => {
         let rmoves = [-8, -1, 1, 8]
         let moves = [];
         const originalRank = Math.floor((id - 1) / 8);
@@ -152,7 +152,7 @@ export default function Board(props) {
                     break;
                 }
                 if (containsColorPiece(target, white) || containsColorPiece(target, !white)) {
-                    if (containsColorPiece(target, !white)) {
+                    if (containsColorPiece(target, !white) || checking) {
                         moves.push(target);
                     }
                     hitPiece = true;
@@ -165,13 +165,13 @@ export default function Board(props) {
     }
 
 
-    const getKnightMoves = (id, white) => {
+    const getKnightMoves = (id, white, checking) => {
         const knmoves = [-17, -15, -10, -6, 6, 10, 15, 17];
         let moves = [];
         for (let i = 0; i < knmoves.length; i++) {
             const target = id + knmoves[i];
             const didntCross = Math.abs((((id % 8) === 0) ? 8 : (id % 8)) - (((target % 8) === 0) ? 8 : (target % 8))) <= 2;
-            if (target > 0 && target <= 64 && !containsColorPiece(target, white) && didntCross) {
+            if (target > 0 && target <= 64 && (!containsColorPiece(target, white) || checking) && didntCross) {
                 moves.push(target);
             }
         }
@@ -179,7 +179,7 @@ export default function Board(props) {
         
     }
 
-    const getBishopMoves = (id, white) => {
+    const getBishopMoves = (id, white, checking) => {
         let bmoves = [-9, -7, 7, 9]
         let moves = [];
         for (let i = 0; i < bmoves.length; i++) {
@@ -192,7 +192,7 @@ export default function Board(props) {
                     break;
                 }
                 if (containsColorPiece(target, white) || containsColorPiece(target, !white)) {
-                    if (containsColorPiece(target, !white)) {
+                    if (containsColorPiece(target, !white) || checking) {
                         moves.push(target);
                     }
                     hitPiece = true;
@@ -204,17 +204,17 @@ export default function Board(props) {
         return moves;
     }
 
-    const getQueenMoves = (id, white) => {
-        return getRookMoves(id, white).concat(getBishopMoves(id, white));
+    const getQueenMoves = (id, white, checking) => {
+        return getRookMoves(id, white, checking).concat(getBishopMoves(id, white, checking));
     }
 
-    const getKingMoves = (id, white) => {
+    const getKingMoves = (id, white, checking) => {
         const kmoves = [-9, -8, -7, -1, 1, 7, 8, 9];
         let moves = [];
         for (let i = 0; i < kmoves.length; i++) {
             const target = id + kmoves[i];
             const didntCross = Math.abs((((id % 8) === 0) ? 8 : (id % 8)) - (((target % 8) === 0) ? 8 : (target % 8))) <= 1;
-            if (target > 0 && target <= 64 && !containsColorPiece(target, white) && didntCross) {
+            if (target > 0 && target <= 64 && (!containsColorPiece(target, white) || checking) && didntCross) {
                 moves.push(target);
             }
         }
@@ -222,22 +222,22 @@ export default function Board(props) {
         
     }
 
-    // returns the ids of squares that a piece on id of given color could move to.
-    const getMoves = (id, white) => {
+    // returns the ids of squares that a piece on id of given color could move to. checking is true if we are checking for check
+    const getMoves = (id, white, checking) => {
         const piece = boardState[id - 1].toLowerCase();
         switch (piece) {
             case 'p':
-                return getPawnMoves(id, white);
+                return getPawnMoves(id, white, checking);
             case 'r':
-                return getRookMoves(id, white);
+                return getRookMoves(id, white, checking);
             case 'n':
-                return getKnightMoves(id, white);
+                return getKnightMoves(id, white, checking);
             case 'b':
-                return getBishopMoves(id, white);
+                return getBishopMoves(id, white, checking);
             case 'q':
-                return getQueenMoves(id, white);
+                return getQueenMoves(id, white, checking);
             case 'k':
-                return getKingMoves(id, white);
+                return getKingMoves(id, white, checking);
             default:
                 return [];
         }
@@ -282,7 +282,18 @@ export default function Board(props) {
             } else if (containsColorPiece(id, true)) {
                 setValidSelection(true);
                 console.log(boardState[id - 1].toString().toLowerCase());
-                setHighlightedSquares(getMoves(id, true));
+                let moves = getMoves(id, true, false);
+                for (let i = 0; i < moves.length; i++) {
+                    let newBoardState = [...boardState];
+                    newBoardState[moves[i] - 1] = newBoardState[id - 1];
+                    newBoardState[id - 1] = " ";
+                    if (inCheck(newBoardState, true)) {
+                        console.log("well fuck");
+                        moves.splice(i, 1);
+                        i--;
+                    }
+                }
+                setHighlightedSquares(moves);
                 return id;
 
             } else {
@@ -309,12 +320,6 @@ export default function Board(props) {
         blackPerformMove(sourceid, targetid);
         setMoved(true);
         setValidSelection(false);
-        if (inCheck(boardState, true)) {
-            console.log("somebody in trouuble");
-        }
-        if (inCheck(boardState, false)) {
-            console.log("the other guy in trouuble");
-        }
     }
 
     
